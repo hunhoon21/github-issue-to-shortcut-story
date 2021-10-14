188b4a85b066f12c984d52b892ff2b688ec14333
hunhoon21 <hunhoon21@gmail.com>
2021-10-14 23:48:17 +0900

    :hammer: exploration of github event

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ import json
 
 print(os.environ)
 print("-" * 30)
-with open(os.environ["GITHUB_ENVET_PATH"]) as f:
+with open(os.environ["GITHUB_EVENT_PATH"]) as f:
     data = json.load(f)
 print(f"github_event: {data}")
