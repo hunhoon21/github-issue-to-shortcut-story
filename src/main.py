b82dd9947970523658cb84b033b985c202bc3e3f
hunhoon21 <hunhoon21@gmail.com>
2021-10-15 01:16:17 +0900

    :hammer: only github action work when target label labeled

--- src/main.py
+++ src/main.py
@@ -15,18 +15,21 @@ shortcut_api_token = os.environ["INPUT_SHORTCUT_API_TOKEN"]
 
 print(f"event: {event}")
 
-url = 'https://api.app.shortcut.com/api/v3/stories'
-headers = {
-    'Content-Type': 'application/json',
-    'Shortcut-Token': '61614d17-06ec-4f5f-87c7-3c5f950731c4'
-}
-body = {
-    'name': data["issue"]["title"],
-    'description': data["issue"]["body"],
-    'workflow_state_id': 500000042,  # productization-unscheduled
-    'group_id': '6077d73c-3f22-4a72-9e2b-b79011865fb5',  # Canvas
-    'project_id': 3238  # Canvas
- }
+if data["action"] == "labeled" and data["label"]["name"] == event:
+    url = 'https://api.app.shortcut.com/api/v3/stories'
+    headers = {
+        'Content-Type': 'application/json',
+        'Shortcut-Token': '61614d17-06ec-4f5f-87c7-3c5f950731c4'
+    }
+    body = {
+        'name': data["issue"]["title"],
+        'description': data["issue"]["body"],
+        'workflow_state_id': 500000042,  # productization-unscheduled
+        'group_id': '6077d73c-3f22-4a72-9e2b-b79011865fb5',  # Canvas
+        'project_id': 3238  # Canvas
+    }
 
-resp = requests.post(url, headers=headers, data=json.dumps(body))
-print(resp.json())
+    resp = requests.post(url, headers=headers, data=json.dumps(body))
+    print(resp.json())
+else:
+    print("this action do not create shortcut story card.")
