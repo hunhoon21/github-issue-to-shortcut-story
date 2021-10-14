a0538222f273e0164b5af10f229f0791e99ced37
hunhoon21 <hunhoon21@gmail.com>
2021-10-15 00:30:39 +0900

    :hammer: get event information from GITHUB_EVENT_PATH

--- src/main.py
+++ src/main.py
@@ -13,14 +13,16 @@ print("-" * 30)
 event = os.environ["INPUT_EVENT"]
 shortcut_api_token = os.environ["INPUT_SHORTCUT_API_TOKEN"]
 
+print(f"event: {event}")
+
 url = 'https://api.app.shortcut.com/api/v3/stories'
 headers = {
     'Content-Type': 'application/json',
     'Shortcut-Token': '61614d17-06ec-4f5f-87c7-3c5f950731c4'
 }
 body = {
-    'name': event["issue"]["title"],
-    'description': event["issue"]["body"],
+    'name': data["issue"]["title"],
+    'description': data["issue"]["body"],
     'workflow_state_id': 500000042,  # productization-unscheduled
     'group_id': '6077d73c-3f22-4a72-9e2b-b79011865fb5',  # Canvas
     'project_id': 3238  # Canvas
