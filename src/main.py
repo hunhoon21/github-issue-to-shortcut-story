61feb1b493b13c1b4a839898a4c4eb1dfe54098e
hunhoon21 <hunhoon21@gmail.com>
2021-10-15 17:11:18 +0900

    :hammer: add github token to create github issue comment for recording shortcut story number

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ print("-" * 30)
 
 label_to_create_shortcut_story = os.environ["INPUT_LABEL_TO_CREATE_SHORTCUT_STORY"]
 shortcut_api_token = os.environ["INPUT_SHORTCUT_API_TOKEN"]
+github_token = os.environ["INPUT_GITHUB_TOKEN"]
 
 print(f"label_to_create_shortcut_story: {label_to_create_shortcut_story}")
 
@@ -31,5 +32,22 @@ if data["action"] == "labeled" and data["label"]["name"] == label_to_create_shor
 
     resp = requests.post(url, headers=headers, data=json.dumps(body))
     print(resp.json())
+
+    shortcut_story_id = resp.json()["id"]
+
+    comment_body = f"This Github issue is related Shortcut Story num: {shortcut_story_id}"
+
+    issue_number = data["issue"]["number"]
+    "https://api.github.com/repos/hunhoon21/github-action/issues/13/comments"
+    repo_url = data["issue"]["url"]
+    url = f"{repo_url}/comments"
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {github_token}"
+    }
+    comment_body = {"body": comment_body}
+    resp = requests.post(url, headers=headers, data=json.dumps(comment_body))
+    print(resp.json())
+
 else:
     print("this action do not create shortcut story card.")
